8efff25c3f00194976e306b0aae129b3a9018db2
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-21 21:15:59 +0530

    Add Loginform component and update app structure
    - Created Loginform component with associated HTML, CSS, and spec files.
    - Updated app component to include Loginform in imports and template.
    - Removed placeholder content from app.html to integrate Loginform.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Loginform } from "./login/loginform/loginform";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Loginform],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
