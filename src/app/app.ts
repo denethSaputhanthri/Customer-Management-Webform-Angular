94d892808474fe585c44194dd513806fa9753d5f
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-21 22:22:40 +0530

    Remove Loginform component and add Hamepage component with associated files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Loginform } from "./login/loginform/loginform";
+import { Hamepage } from "./home/hamepage/hamepage";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Loginform],
+  imports: [RouterOutlet, Loginform, Hamepage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
